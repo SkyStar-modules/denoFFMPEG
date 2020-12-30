02ccb2b7de220c029a5cb2dff798f336366d8f99
mierenmanz <christiaanmens@gmail.com>
2020-12-30 22:55:54 +0100

    added filter tests

---
+++ tests/videoFilters_test.ts
@@ -0,0 +1,40 @@
+import { FfmpegClass, Filters } from "../mod.ts";
+const sentence = "I love deno :D"
+const text:Filters = {
+    filterName: "drawtext",
+    options: {
+        text: sentence,
+        fontsize: "60",
+        x: (856 / 2 - 30 * sentence.length / 2),
+        y: "H-240",
+        fontcolor: "white",
+        shadowcolor: "black",
+        shadowx: "2",
+        shadowy: "2",
+    }
+}
+Deno.test({
+    name: " feature",
+    fn: async() => {
+        await new FfmpegClass({
+            ffmpegDir: "./ffmpeg/ffmpeg.exe",
+            source: "./input.mp4"
+        }).videoFilters(text).save("./ree.mp4");
+    },
+    sanitizeOps: true,
+    sanitizeResources: true
+})
+Deno.test({
+    name: " feature with progress",
+    fn: async() => {
+        const thing = new FfmpegClass({
+            ffmpegDir: "./ffmpeg/ffmpeg.exe",
+            source: "./input.mp4"
+        }).videoFilters(text).saveWithProgress("./ree.mp4");
+        for await (const progress of thing) {
+            console.log(progress);
+        }
+    },
+    sanitizeOps: true,
+    sanitizeResources: true
+})
