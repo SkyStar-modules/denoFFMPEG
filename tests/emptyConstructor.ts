c38951f7e55b5f25b9de9d13c4508d8ad5e8156f
mierenmanz <christiaanmens@gmail.com>
2020-12-30 17:50:45 +0100

    fixed typo

--- tests/emptyConstructor.ts
+++ tests/emptyConstructor.ts
@@ -1,12 +1,12 @@
 import { FfmpegClass } from "../mod.ts";
 Deno.test({
-    name: " feature",
+    name: "empty constructor feature",
     fn: async() => {
         await new FfmpegClass().inputFile("./input.mp4").setFfmpegPath("./ffmpeg/ffmpeg.exe").save("./ree.mp4");
     }
 })
 Deno.test({
-    name: " feature with progress",
+    name: "empty constructor feature with progress",
     fn: async() => {
         const thing = new FfmpegClass().inputFile("./input.mp4").setFfmpegPath("./ffmpeg/ffmpeg.exe").saveWithProgress("./ree.mp4");
         for await (const progress of thing) {
