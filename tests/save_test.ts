660f93ee0fba814dce3a4cc90f1add8311dbb986
mierenmanz <christiaanmens@gmail.com>
2021-01-16 23:37:44 +0100

    combined tests into 1 file

--- tests/save_test.ts
+++ tests/save_test.ts
@@ -10,3 +10,17 @@ Deno.test({
     sanitizeOps: true,
     sanitizeResources: true
 })
+Deno.test({
+    name: "SaveWithProgress feature",
+    fn: async() => {
+        const thing = new FfmpegClass({
+            ffmpegDir: "./ffmpeg/ffmpeg.exe",
+            input: "./input.mp4"
+        }).saveWithProgress("./ree.mp4");
+        for await (const progress of thing) {
+            console.log(progress);
+        }
+    },
+    sanitizeOps: true,
+    sanitizeResources: true
+})
