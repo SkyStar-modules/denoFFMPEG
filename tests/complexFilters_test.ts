8aa5302347e8c53598a223bb481f6df7e0228625
mierenmanz <christiaanmens@gmail.com>
2021-01-19 19:08:07 +0100

    upgraded tests to reflex usage better

--- tests/complexFilters_test.ts
+++ tests/complexFilters_test.ts
@@ -1,36 +1,30 @@
 import { FfmpegClass, Filters } from "../mod.ts";
-const sentence = "I love deno :D"
-const text:Filters = {
-    filterName: "drawtext",
+const link = "https://cdn.discordapp.com/attachments/467182812382887936/800066821713297448/zZpCsH2uswobVSd2L2OplA4ad59L2arVxYtsFpJSEc3hqYB5DeyhfosH8VO4hgOUxi9oJHsRNHFsUQEV98au4w.png";
+const overlay:Filters = {
+    complex: true,
+    filterName: "overlay",
     options: {
-        text: sentence,
-        fontsize: "60",
-        x: (856 / 2 - 30 * sentence.length / 2),
-        y: "H-240",
-        fontcolor: "white",
-        shadowcolor: "black",
-        shadowx: "2",
-        shadowy: "2",
+        x: '150'
     }
 }
 Deno.test({
-    name: "simple videoFilter feature",
+    name: "complex videoFilter feature",
     fn: async() => {
         await new FfmpegClass({
             ffmpegDir: "./ffmpeg/ffmpeg.exe",
             input: "./input.mp4"
-        }).videoFilters(text).save("./ree.mp4");
+        }).addInput(link).complexFilters(overlay).save("./ree.mp4");
     },
     sanitizeOps: true,
     sanitizeResources: true
 })
 Deno.test({
-    name: "simple videoFilter feature with progress",
+    name: "complex videoFilter feature with progress",
     fn: async() => {
         const thing = new FfmpegClass({
             ffmpegDir: "./ffmpeg/ffmpeg.exe",
             input: "./input.mp4"
-        }).videoFilters(text).saveWithProgress("./ree.mp4");
+        }).addInput(link).complexFilters(overlay).saveWithProgress("./ree.mp4");
         for await (const progress of thing) {
             console.log(progress);
         }
--- tests/videoCodec_test.ts
+++ tests/videoCodec_test.ts
@@ -1,11 +1,15 @@
 import { FfmpegClass } from "../mod.ts";
+const options = {
+    preset: "fast",
+    tune: "zerolatency"
+}
 Deno.test({
     name: "videoCodec feature",
     fn: async() => {
         await new FfmpegClass({
             ffmpegDir: "./ffmpeg/ffmpeg.exe",
             input: "./input.mp4"
-        }).videoCodec("libx264").save("./ree.mp4");
+        }).videoCodec("libx264", options).save("./ree.mp4");
     },
     sanitizeOps: true,
     sanitizeResources: true
@@ -16,7 +20,7 @@ Deno.test({
         const thing = new FfmpegClass({
             ffmpegDir: "./ffmpeg/ffmpeg.exe",
             input: "./input.mp4"
-        }).videoCodec("libx264").saveWithProgress("./ree.mp4");
+        }).videoCodec("libx264", options).saveWithProgress("./ree.mp4");
         for await (const progress of thing) {
             console.log(progress);
         }
