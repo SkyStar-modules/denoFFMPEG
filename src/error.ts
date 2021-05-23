53f47cd4e7b359aebe306d9891608d86d1fa78b3
MierenManz <christiaanmens@gmail.com>
2021-05-23 22:54:29 +0200

    fix color issue

--- src/error.ts
+++ src/error.ts
@@ -1,27 +1,27 @@
 export function warning(str: string): void {
-  return console.warn("\x1b[0;33;40mWARNING:\x1b[39m " + str);
+  return console.warn("\x1b[33mWARNING:\x1b[0m " + str);
 }
 
 export function internalWarning(str: string): void {
-  return console.warn("\x1b[0;33;40mINTERNAL WARNING:\x1b[39m " + str);
+  return console.warn("\x1b[33mINTERNAL WARNING:\x1b[0m " + str);
 }
 
 export class InternalError extends Error {
-  name = "\x1b[0;31;40mINTERNAL ERROR:\x1b[39m";
+  name = "\x1b[31mINTERNAL ERROR:\x1b[0m";
   constructor(errorMessage: string) {
     super(errorMessage);
   }
 }
 
 export class FfmpegError extends Error {
-  name = "\x1b[0;31;40mRENDER DIDN'T FINISH:\x1b[39m";
+  name = "\x1b[31mRENDER DIDN'T FINISH:\x1b[0m";
   constructor(errorMessage: string) {
     super(errorMessage);
   }
 }
 
 export class FormatError extends Error {
-  name = "\x1b[0;31;40mFormatting Error:\x1b[39m";
+  name = "\x1b[31mFormatting Error:\x1b[0m";
   constructor(errorMessage: string) {
     super(errorMessage);
   }
