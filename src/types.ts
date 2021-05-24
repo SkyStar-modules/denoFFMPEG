fc99f5243ed804596b2ba658346ea640640d9182
MierenManz <christiaanmens@gmail.com>
2021-05-24 11:46:31 +0200

    add extra type for piped data

--- src/types.ts
+++ src/types.ts
@@ -3,16 +3,25 @@ export interface Filters {
   filterName: string;
   options: Record<string, string | number>;
 }
+
 export interface Spawn {
   threads?: number;
   ffmpegDir?: string;
   niceness?: number;
   input?: string;
 }
+
 export interface Progress {
   ETA: Date;
   percentage: number;
 }
+
+export interface ProgressPiped {
+  ETA: Date;
+  percentage: number;
+  pipedData: Uint8Array;
+}
+
 export interface Globals {
   ffmpegdir: string;
   niceness: number;
