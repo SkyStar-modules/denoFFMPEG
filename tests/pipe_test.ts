cd79ab26780971763eeec8ab6f1bd7bc33af1793
MierenManz <christiaanmens@gmail.com>
2021-05-25 17:42:54 +0200

    fix: tests & ci

--- tests/pipe_test.ts
+++ tests/pipe_test.ts
@@ -1,12 +1,12 @@
 import { FfmpegClass } from "../mod.ts";
-const data = await Deno.readFile("./tests/videos/input.ts");
+const data = await Deno.readFile("./tests/videos/input.webm");
 
 Deno.test({
   name: "input pipe feature",
   fn: async () => {
     await new FfmpegClass({
       ffmpegDir: "ffmpeg",
-    }).addInput(data, { f: "mpegts" }).save("./tests/videos/output.mp4");
+    }).addInput(data).save("./tests/videos/output.mp4");
   },
   sanitizeOps: true,
   sanitizeResources: true,
