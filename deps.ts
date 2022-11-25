82ad8ae44d5bbd2b610a1b57306eb297a775392c
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-11-25 10:13:58 -0800

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { readLines } from "https://deno.land/std@0.97.0/io/bufio.ts";
-export { writeAll } from "https://deno.land/std@0.97.0/io/util.ts";
+export { readLines } from "https://deno.land/std@0.166.0/io/buffer.ts";
+export { writeAll } from "https://deno.land/std@0.166.0/streams/conversion.ts";
