217f75970e80dc2dbad2669998caaf14ce45138b
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-12-15 13:30:02 -0800

    fix: bumped dependencies and fixed link

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { readLines } from "https://deno.land/std@0.166.0/io/buffer.ts";
-export { writeAll } from "https://deno.land/std@0.166.0/streams/conversion.ts";
+export { readLines } from "https://deno.land/std@0.168.0/io/buffer.ts";
+export { writeAll } from "https://deno.land/std@0.168.0/streams/conversion.ts";
